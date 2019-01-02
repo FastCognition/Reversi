2802b5fe4b1feeec8d7a1b4deea2a785e26659c5
PracticalsUU <46297141+PracticalsUU@users.noreply.github.com>
2019-01-02 13:00:24 +0100

    Update Reversi.cs

--- Reversi.cs
+++ Reversi.cs
@@ -1,1 +1,29 @@
-ss
+void Plaatsstenen(int nieuwex, int nieuwey, Piece color)
+        {
+            for (int dx = -1; dx <= 1; dx++)
+            {
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    {
+                        try
+                        {
+                            int teller = 1;
+                            while (stenen[nieuwex + (dx  teller), nieuwey + (dy  teller)] != Piece.color)
+                            {
+                                teller++;
+                            }
+                            if (stenen[nieuwex + (dx  teller), nieuwey + (dy  teller)] == Piece.color)
+                            {
+                                while (teller >= 1)
+                                {
+                                    stenen[nieuwex + (dx  teller), nieuwey + (dy  teller)] = Piece.color;
+                                    teller--;
+                                }
+                            }
+                        }catch (IndexOutOfRangeException e) { }
+                    }
+                }
+            }
+            beurt++;
+            //huidigespelstatus = huidigespelstatus == "Zwart" ? "Wit" : "Zwart";
+        }
